9d80f2a682f9973aec971826a2869180d5dd265a
ArtyomR102 <artyomr102@gmail.com>
2025-10-11 14:18:48 +0300

    Added footer

---
+++ assets/scripts/counter.js
@@ -0,0 +1,13 @@
+void initCounter()
+{
+    let d = document;
+    let s = screen;
+    if (new URLSearchParams(window.location.search).has('noCounter'))
+        d.getElementById("counter").parentElement.remove();
+    else
+        d.getElementById("counter").src = "https://counter.yadro.ru/hit?t18.1;r"
+        + escape(d.referrer) + ((typeof(s)=="undefined") ? "" : ";s"
+        + s.width + "*" + s.height + "*" + (s.colorDepth ? s.colorDepth : s.pixelDepth))
+        + ";u"+escape(d.URL) + ";h" + escape(d.title.substring(0,150)) + ";" + Math.random();
+}
+
