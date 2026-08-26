058553ff6a1eae2d84f6cbb02500a352bdff38b7
ArtyomR_102 <78036453+ArtyomR102@users.noreply.github.com>
2026-08-27 02:05:36 +0300

    Hotfix 5

--- assets/scripts/vacuum-energy-sim.js
+++ assets/scripts/vacuum-energy-sim.js
@@ -1,13 +1,13 @@
 const VacuumEnergySim = {
 	pal: [
-		[0,0,0],       // 0 - ложный вакуум (чёрный)
-		[255,0,0],     // 1 - уровень 1 (красный)
-		[0,255,0],     // 2 - уровень 2 (зелёный)
-		[0,0,255],     // 3 - уровень 3 (синий)
-		[255,255,0],   // 4 - жёлтый
-		[255,0,255],   // 5 - пурпурный
-		[0,255,255],   // 6 - циан
-		[255,255,255]  // 7 - белый (максимум)
+		[0, 0, 0],
+		[255, 0, 0],
+		[0, 255, 0],
+		[0, 0, 255],
+		[255, 255, 0],
+		[255, 0, 255],
+		[0, 255, 255],
+		[255, 255, 255]
 	],
 	
 	initialize: function() {
@@ -31,37 +31,21 @@ const VacuumEnergySim = {
 	},
 	
 	tick: function() {
-		let e_new = this.e.map(row => new Float32Array(row));
-		
-		for (let y = 1; y < this.height - 1; y++) {
-			for (let x = 1; x < this.width - 1; x++) {
-				let straight = Math.max(
-					this.e[y-1][x], this.e[y+1][x],
-					this.e[y][x-1], this.e[y][x+1]
+		for (let x = 1; x < this.width - 1; x++) {
+			for (let y = 1; y < this.height - 1; y++) {
+				this.new[y][x] = Math.max(
+					Math.ceil(this.e[y][x]),
+					Math.max(
+						this.e[y-1][x], this.e[y+1][x],
+						this.e[y][x-1], this.e[y][x+1]
+					),
+					Math.max(
+						this.e[y-1][x-1], this.e[y-1][x+1],
+						this.e[y+1][x-1], this.e[y+1][x+1]
+					) - 0.707
 				);
-				let diag = Math.max(
-					this.e[y-1][x-1], this.e[y-1][x+1],
-					this.e[y+1][x-1], this.e[y+1][x+1]
-				);
-				
-				e_new[y][x] = Math.max(
-					this.e[y][x],
-					straight,
-					diag * 0.707
-				);
-			}
-		}
-		
-		this.e = e_new;
-		
-		let data = this.img.data;
-		for (let y = 0; y < this.height; y++) {
-			for (let x = 0; x < this.width; x++) {
-				let level = Math.ceil(this.e[y][x]);
-				level = Math.min(level, this.pal.length - 1);
-				
 				let idx = (y * this.width + x) * 4;
-				let color = this.pal[level];
+				let color = this.pal[Math.floor(this.e[x][y]) % 8];
 				data[idx] = color[0];
 				data[idx+1] = color[1];
 				data[idx+2] = color[2];
@@ -69,7 +53,6 @@ const VacuumEnergySim = {
 			}
 		}
 		this.ctx.putImageData(this.img, 0, 0);
-		
 		requestAnimationFrame(() => this.tick());
 	}
 };
